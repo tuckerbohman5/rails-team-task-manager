43cacb0f72c43c7ff481f874c0e09efff169c3a8
tuckerbohman5 <tucker.bohman@gmail.com>
2016-03-19 17:11:37 -0600

    signup functionality working

---
+++ team_task_manager/app/controllers/sessions_controller.rb
@@ -0,0 +1,16 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+  
+  def create
+    @user = User.find_by(name: params[:user][:name])
+
+    if @user && @user.authenticate(params[:user][:password])
+      session[:user_id] = @user.id 
+      redirect_to root_path
+    else
+      redirect_to login_path
+    end
+  end
+end
