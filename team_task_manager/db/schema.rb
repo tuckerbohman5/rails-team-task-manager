0fa5ef40249ddc353a5ee990765314afc94f858b
tuckerbohman5 <tucker.bohman@gmail.com>
2016-03-26 15:31:23 -0600

    added completed boolean to task

--- team_task_manager/db/schema.rb
+++ team_task_manager/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160320204928) do
+ActiveRecord::Schema.define(version: 20160326212922) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "content"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20160320204928) do
     t.integer  "user_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.boolean  "completed"
   end
 
   create_table "users", force: :cascade do |t|
