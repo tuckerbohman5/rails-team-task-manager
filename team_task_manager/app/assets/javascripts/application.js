0f584d068b3a349e2715b272218f6d4e75564a72
tuckerbohman5 <tucker.bohman@gmail.com>
2016-04-30 09:58:13 -0600

    javascript is working after creating ananymous functions

--- team_task_manager/app/assets/javascripts/application.js
+++ team_task_manager/app/assets/javascripts/application.js
@@ -14,11 +14,144 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require bootstrap-sprockets
-//= require app
 //= require_tree .
 
+$(document).ready(function() {
+  attachListeners();
+});
+
+  function attachListeners(){
+    $('.hideInfo').hide();
+    $('#hideComments').hide();
+    $('#loadComments').on('click', function(){
+      loadComments();
+    });
+    $('form').on('submit', function(event){
+      createComment(event);
+    });
+    $('#hideComments').on('click', function(){
+      hideComments();
+    });
+    $('.moreInfo').on('click', function(){
+      getMoreInfo();
+    }); 
+    $('.hideInfo').on('click', function(){
+      hideMoreInfo();
+    });
+    $('#viewUsers').on('click', function(){
+      viewUsers();
+    });
+    $('#hideUsers').on('click', function(){
+      hideUsers();
+    });
+  }
 
 
-// $('#loadComments').on('submit', function(){
-//   console.log("loadingComments...");
-// })
+  function loadComments() {
+    $('#projectComments').html('<h5>Comments:</h5>')
+    $.ajax({
+      method: 'GET',
+      url: this.href,
+      dataType: 'JSON'
+    }).done(function(projectData){
+      extractComments(projectData);
+    })
+  }
+
+  function extractComments(projectData) {
+    var project = projectData["project"];
+    var comments = project["comments"];
+      for (var i = 0; i < comments.length; i++) {
+        var commentData = comments[i];
+        var commentHtml = '';
+        commentHtml = buildCommentParagraph(commentData);
+        $('#projectComments').append(commentHtml);
+      }
+    $('#loadComments').hide();
+    $('#hideComments').show();
+  }
+
+  function buildCommentParagraph(commentData){
+    return "<p>" + commentData["user"]["name"] + " said " + commentData["content"] + "</p>";
+  }
+
+  function createComment(event) {
+    event.preventDefault();
+    var commentData = $(this).serialize();
+    $.ajax({
+      method: 'POST',
+      url: '/comments',
+      data: commentData
+    }).done(function(data){
+      appendComment(data);
+    })
+  }
+
+  function appendComment(data) {
+    debugger;
+  }
+
+  function hideComments() {
+    $('#projectComments').html('');
+    $('#loadComments').show();
+    $(this).hide();
+  }
+  
+  function getMoreInfo() {
+    $.ajax({
+      method: 'GET',
+      url: '/projects/' + this.attributes[1].value,
+      dataType: 'JSON'
+    }).done(function(projectData){
+      extractInfo(projectData);
+    });
+  }
+
+  function extractInfo(projectData){
+    var id = projectData["project"]["id"];
+    var description = projectData["project"]["description"];
+    $('p[data-id="'+ id +'"]').text(description);
+    $('a.moreInfo[data-id="'+ id +'"]').hide();
+    $('a.hideInfo[data-id="'+ id +'"]').show();
+  }
+
+  function hideMoreInfo() {
+    var id = this.attributes[1].value;
+    $(this).hide();
+    $('p[data-id="'+ id +'"]').text('');
+    $('a.moreInfo[data-id="'+ id +'"]').show();  
+  } 
+
+  function viewUsers() {
+    $('#viewUsers').hide();
+    $('#hideUsers').show();
+    $.ajax({ 
+      method: 'GET',
+      url: '/users',
+      dataType: 'JSON'
+    }).done(function(usersData){
+      extractUsers(usersData);
+    });
+  }
+  
+  function extractUsers(usersData) {
+    var users = usersData["users"];
+    for(var i = 0; i < users.length; i++){
+      var user = {
+        name: users[i]["name"],
+        taskCount: users[i]["tasks"].length
+      }
+      appendUser(user);
+    }
+  }
+
+  function appendUser(user) {
+    var usersDiv = $('#users');
+    usersDiv.append('<p>' + user.name + ' has ' + user.taskCount + ' tasks!</p>');
+  }
+
+  function hideUsers() {
+    $('#users').html('');
+    $('#viewUsers').show();
+    $('#hideUsers').hide();
+  }
